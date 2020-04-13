3b82a83cd76152e6829211fdb0c8bffef29d81c1
Justin Winger <justinwinger4@gmail.com>
2020-04-13 12:59:52 -0600

    Plant file setup

--- lib/gardener/plant.rb
+++ lib/gardener/plant.rb
@@ -1,5 +1,20 @@
 class Plant
   attr_accessor :name, :plant_id
+  @@all = []
+  
+  def initialize(name:, plant_id)
+    @name = name 
+    @drink_id = drink_id 
+    @@all << self
+  end
+
+  def self.all 
+    @@all
+  end
+
+  def self.find_by_name(name)
+    @@all.select {|n| n.name == name}
+  end
 
 
 end
