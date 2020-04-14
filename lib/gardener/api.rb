d4eea8f7a51ee49b77f01cdc693697635a5a5608
Justin Winger <justinwinger4@gmail.com>
2020-04-14 11:02:43 -0600

    Added get_instructions

--- lib/gardener/api.rb
+++ lib/gardener/api.rb
@@ -22,8 +22,16 @@ class Api
         response = Net::HTTP.get_response(uri)
         plants_objects = JSON.parse(response.body)
         # binding.pry
-            plants_objects[plant]["description"] + plants_objects[plant]["optimal_sun"] + plants_objects[plant]["optimal_soil"] + plants_objects[plant]["planting_considerations"] + plants_objects[plant]["when_to_plant"] + plants_objects[plant]["growing_from_seed"] + plants_objects[plant]["transplanting"] + plants_objects[plant]["spacing"] + plants_objects[plant]["watering"] + plants_objects[plant]["feeding"] + plants_objects[plant]["other_care"] + plants_objects[plant]["diseases"] + plants_objects[plant]["harvesting"] + plants_objects[plant]["storage_use"]
+            plants_objects[plant]["description"]
   
+    end
+
+    def self.get_instructions(plant)
+        url = "http://harvesthelper.herokuapp.com/api/v1/plants?api_key=2e7da5303df06d151a7aeb1d403aa7b2"
+        uri = URI.parse(url)
+        response = Net::HTTP.get_response(uri)
+        plants_objects = JSON.parse(response.body)
+        plants_objects[plant]["optimal_sun"] + plants_objects[plant]["optimal_soil"] + plants_objects[plant]["planting_considerations"] + plants_objects[plant]["when_to_plant"] + plants_objects[plant]["growing_from_seed"] + plants_objects[plant]["transplanting"] + plants_objects[plant]["spacing"] + plants_objects[plant]["watering"] + plants_objects[plant]["feeding"] + plants_objects[plant]["other_care"] + plants_objects[plant]["diseases"] + plants_objects[plant]["harvesting"] + plants_objects[plant]["storage_use"]
     end
 end
 
