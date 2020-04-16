4b818e30edd9e1cf3b76d753fed3512608f10177
Justin Winger <justinwinger4@gmail.com>
2020-04-16 08:22:15 -0600

    removed comments

--- lib/gardener/api.rb
+++ lib/gardener/api.rb
@@ -1,10 +1,4 @@
 class Api 
-
-    # PERSONAL_ACCESS_TOKEN = ENV["2e7da5303df06d151a7aeb1d403aa7b2"]
-    # ACCOUNT_ID = ENV["justinwinger4@gmail.com"]
-    # "http://harvesthelper.herokuapp.com/api/v1/plants?api_key=2e7da5303df06d151a7aeb1d403aa7b2"
-
-
     def self.get_plants
         url = "http://harvesthelper.herokuapp.com/api/v1/plants?api_key=2e7da5303df06d151a7aeb1d403aa7b2"
         uri = URI.parse(url)
